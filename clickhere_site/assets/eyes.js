4e2f0e72a7331000bb1c0a49d68a0633db90c00d
mr1640 <mr1640@rutgers.edu>
2022-10-20 23:40:23 -0400

    adjusted eyes, fixed flies

--- clickhere_site/assets/eyes.js
+++ clickhere_site/assets/eyes.js
@@ -1,71 +1,57 @@
 function setup() {
-    createCanvas(windowWidth/.7, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+  
+  let x = 0;
+  let y = 0;
+  
+  while (x < width && y < height) {
+    // statements
+     let mover = x*100;
+    let movery = y*100;
+     
+
+  eyeball(mouseX,mouseY,0,mover,255);
+  eyeball(mouseX/2,mouseY,windowWidth/4,mover,255);
+  eyeball(mouseX/3,mouseY,windowWidth/2,mover,255);
+  eyeball(mouseX,mouseY/2,0,windowHeight/4,255);
+  eyeball(mouseX/2,mouseY/2,windowWidth/4,windowHeight/4,255);
+  eyeball(mouseX/3,mouseY/2,windowWidth/2,windowHeight/4,255);
+    x += 100;
+    y += 100;
   }
   
-  function draw() {
-    background(0);
-    //whites ofeye
-    noStroke();
-    fill(255);
-    circle(height/2,height/2,windowHeight/2.5);
-    //iris
-    let xc =constrain(mouseX, windowHeight/2.3, windowHeight/1.8);
-    let xs = constrain(mouseY, windowHeight/2.5,windowHeight/2);
-    fill(0);
-    circle(xc,xs,windowHeight/5);
-    //glare
-    fill(255);
-    circle(xc+20,xs-20,windowHeight/25);
-    
-   
-    
-    translate(windowWidth/3.7,0);
-    
-  
-    //whites ofeye
-    noStroke();
-    fill(255);
-    circle(height/2,height/2,windowHeight/2.5);
-    //iris
-     let xc2 =constrain(mouseX-700, windowHeight/2.3, windowHeight/1.8);
-    let xs2 = constrain(mouseY, windowHeight/2.5,windowHeight/2);
-     fill(0);
-    circle(xc2,xs2,windowHeight/5);
-    //glare
-    fill(255);
-    circle(xc2+20,xs2-20,windowHeight/25);
-    
-      translate(windowWidth/3.7,0);
-    
-  
-    //whites ofeye
-    noStroke();
-    fill(255);
-    circle(height/2,height/2,windowHeight/2.5);
-    //iris
-    let xc3 =constrain(mouseX-900, windowHeight/2.3, windowHeight/1.8);
-    let xs3 = constrain(mouseY, windowHeight/2.5,windowHeight/2);
-    fill(0);
-    circle(xc3,xs3,windowHeight/5);
-    //glare
-    fill(255);
-    circle(xc3+20,xs3-20,windowHeight/25);
-    
-        translate(windowWidth/4.5,0);
-    
-  
-    //whites ofeye
-    noStroke();
-    fill(255);
-    circle(height/2,height/2,windowHeight/2.5);
-    //iris
-    fill(0);
-    circle(xc,xs,windowHeight/5);
-    //glare
-    fill(255);
-    circle(xc+20,xs-20,windowHeight/25);
-    
-  
-    
-  
-  }
+// for(let x = 0;x<width;x+=100
+//    y = 0;x<width;x+=100) 
+//     {
+//  let mover = x*100 
+
+//   eyeball(mouseX,mouseY,0,mover,255);
+//   eyeball(mouseX/2,mouseY,windowWidth/4,mover,255);
+//   eyeball(mouseX/3,mouseY,windowWidth/2,mover,255);
+//   eyeball(mouseX/3,mouseY,windowWidth/2,mover,255);
+// }
+ 
+
+}
+
+function eyeball(mx,my,tx,ty,col){
+  push()
+  translate(tx,ty)  
+  //whites ofeye
+  noStroke();
+  fill(col);
+  circle(height/4,height/4,windowHeight/5);
+  //iris
+  let xc =constrain(mx, windowHeight/4.5, windowHeight/3.35);
+  let xs = constrain(my, windowHeight/4.5,windowHeight/3.35);
+  fill(0);
+  circle(xc,xs,windowHeight/10);
+  //glare
+  fill(255);
+  circle(xc+20,xs-20,windowHeight/25);
+  pop()
+}
