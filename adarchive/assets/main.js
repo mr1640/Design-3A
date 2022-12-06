672b533ba63a3fa9ea442eeef3cdbcbad3df6ce0
mr1640 <mr1640@rutgers.edu>
2022-12-06 10:12:24 -0500

    changed title on archive, changed images on OYE

--- adarchive/assets/main.js
+++ adarchive/assets/main.js
@@ -1,29 +1,29 @@
-// var body = document.getElementsByTagName('body')[0];
-//         body.style.backgroundColor = 'green';
+var body = document.getElementsByTagName('body')[0];
+        body.style.backgroundColor = 'green';
 
-//         // trigger this function every time the user scrolls
-//         window.onscroll = function (event) {
-//             var scroll = window.pageYOffset;
-//             if (scroll < 300) {
-//                 // green
-//                 body.style.backgroundColor = 'green';
-//             } else if (scroll >= 300 && scroll < 600) {
-//                 // yellow
-//                 body.style.backgroundColor = 'yellow';
-//             } else if (scroll >= 600 && scroll < 1200) {
-//                 // blue
-//                 body.style.backgroundColor = 'blue';
-//             } else if (scroll >= 1200 && scroll < 1800) {
-//                 // orange
-//                 body.style.backgroundColor = 'orange';
-//             } else if (scroll >= 1800 && scroll < 3000) {
-//                 // red
-//                 body.style.backgroundColor = 'red';
-//             } else {
-//                 // purple
-//                 body.style.backgroundColor = 'purple';
-//             }
-//         }
+        // trigger this function every time the user scrolls
+        window.onscroll = function (event) {
+            var scroll = window.pageXOffset;
+            if (scroll < 6000) {
+                // green
+                body.style.backgroundColor = 'green';
+            } else if (scroll >= 600 && scroll < 12000) {
+                // yellow
+                body.style.backgroundColor = 'yellow';
+            } else if (scroll >= 12000 && scroll < 20000) {
+                // blue
+                body.style.backgroundColor = 'blue';
+            } else if (scroll >= 20000 && scroll < 25000) {
+                // orange
+                body.style.backgroundColor = 'orange';
+            } else if (scroll >= 25000 && scroll < 450000) {
+                // red
+                body.style.backgroundColor = 'red';
+            } else {
+                // purple
+                body.style.backgroundColor = 'purple';
+            }
+        }
    
 
 
