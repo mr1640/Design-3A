8708a933f449e0ef1effbe53dcf5ff375d88bbfd
mr1640 <mr1640@rutgers.edu>
2022-12-06 12:31:24 -0500

    fixed color on archive, messed with button and cursor OYE

--- adarchive/assets/main.js
+++ adarchive/assets/main.js
@@ -4,24 +4,40 @@ var body = document.getElementsByTagName('body')[0];
         // trigger this function every time the user scrolls
         window.onscroll = function (event) {
             var scroll = window.pageXOffset;
-            if (scroll < 6000) {
+            if (scroll < 10000) {
                 // green
-                body.style.backgroundColor = 'green';
-            } else if (scroll >= 600 && scroll < 12000) {
+                body.style.backgroundColor = '#001118';
+            } else if (scroll >= 10000 && scroll < 20000) {
                 // yellow
-                body.style.backgroundColor = 'yellow';
-            } else if (scroll >= 12000 && scroll < 20000) {
+                body.style.backgroundColor = '#015f73';
+            } else if (scroll >= 20000 && scroll < 30000) {
                 // blue
-                body.style.backgroundColor = 'blue';
-            } else if (scroll >= 20000 && scroll < 25000) {
+                body.style.backgroundColor = '#089396';
+            } else if (scroll >= 30000 && scroll < 40000) {
                 // orange
-                body.style.backgroundColor = 'orange';
-            } else if (scroll >= 25000 && scroll < 450000) {
+                body.style.backgroundColor = '#93d2bd';
+            } else if (scroll >= 40000 && scroll < 50000) {
                 // red
-                body.style.backgroundColor = 'red';
-            } else {
+                body.style.backgroundColor = '#e8d7a6';
+            }
+            else if (scroll >= 50000 && scroll < 60000) {
+                // red
+                body.style.backgroundColor = '#ed9b00';
+            }else if (scroll >= 60000 && scroll < 70000) {
+                // red
+                body.style.backgroundColor = '#ca6701';
+            } else if (scroll >= 70000 && scroll < 80000) {
+                // red
+                body.style.backgroundColor = '#bb3d03';
+            }  else if (scroll >= 80000 && scroll < 90000) {
+                // red
+                body.style.backgroundColor = '#ad2111';
+            }else if (scroll >= 90000 && scroll < 100000) {
+                // red
+                body.style.backgroundColor = '#992125';
+            }else {
                 // purple
-                body.style.backgroundColor = 'purple';
+                body.style.backgroundColor = '#992125';
             }
         }
    
